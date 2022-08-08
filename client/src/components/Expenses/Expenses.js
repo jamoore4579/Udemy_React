ade4a5c5813b688c11f5c91a619c7d7499e81839
Irish Moore <jasonamoore@frontier.com>
2022-08-08 09:51:28 -0400

    setup list logic

--- client/src/components/Expenses/Expenses.js
+++ client/src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, {useState} from 'react'
 
-import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
+import ExpensesList from './ExpensesList'
 
 const Expenses = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020')
@@ -15,7 +15,7 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear
     })
-    
+
     return (
         <div>
             <Card className="expenses">
@@ -23,18 +23,7 @@ const Expenses = (props) => {
                     selected={filteredYear} 
                     onChangeFilter={filterChangeHandler} 
                 />
-                {filteredExpenses.length === 0 ? (
-                    <p>No expenses found</p>
-                ) : (
-                    filteredExpenses.map((expense) => (
-                        <ExpenseItem
-                            key={expense.id}
-                            title={expense.title} 
-                            amount={expense.amount} 
-                            date={expense.date}
-                        />
-                    ))
-                )}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
         </div>
     )    
